7f7b9b998c660929f0770bb0f94343174a07dc64
hooninpark <hooninpark@gmail.com>
2023-07-31 18:55:13 +0900

    python project for control rigging

---
+++ RobotArms.py
@@ -0,0 +1,4 @@
+import unreal
+
+unreal.load_module('ControlRigDeveloper')
+rig = unreal.load_object(name='/Game/ControlRig/Samples/Mannequin_ControlRig', outer=None)
---
+++ hello.py
@@ -0,0 +1,11 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # hello.py
+... print("Hello World")
+... 
+... """
+... Author: EungYong Park
+... Date: 2023-05-01
+... 이 프로그램은 Hello World를 출력하는 프로그램이다.
+... """
+... 
