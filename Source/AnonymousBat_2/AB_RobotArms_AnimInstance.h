83f0d12c9405d2784fc40c428a0e7402755d7b72
hooninpark <hooninpark@gmail.com>
2023-08-04 16:54:06 +0900

    AB_RobotArms_AnimInstance class added and connected to BP

---
+++ Source/AnonymousBat_2/AB_RobotArms_AnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "AB_RobotArms_AnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANONYMOUSBAT_2_API UAB_RobotArms_AnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
